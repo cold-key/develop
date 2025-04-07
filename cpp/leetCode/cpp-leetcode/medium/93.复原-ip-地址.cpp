a43a9eca2d8bf60ba97941dfccba8f747e5ed329
dengbin <2776347262@qq.com>
2025-04-07 22:01:11 +0800

    leetcode 有点小bug，这题应该用递归做的，现在是递推

--- cpp/leetCode/cpp-leetcode/medium/93.复原-ip-地址.cpp
+++ cpp/leetCode/cpp-leetcode/medium/93.复原-ip-地址.cpp
@@ -15,7 +15,39 @@ public:
 
     vector<string> restoreIpAddresses(string s) {
         vector<string> result;
+        for(int i = 1; i <= 3; i++){
+            string firstStr = s.substr(0,i);
+            int firstStrNum = stoi(firstStr);
+            if(firstStrNum < f[i-1][0] || firstStrNum > f[i-1][1]){
+                continue;
+            }
+            for(int j = 1; j <= 3; j++){
+                string secondStr = s.substr(i,j);
+                int secondNum = stoi(secondStr);
+                if(secondNum < f[j-1][0] || secondNum > f[j-1][1]){
+                    continue;
+                }
+                for(int k = 1; k <= 3; k++){
+                    string sanStr = s.substr(i+j,k);
+                    int sanStrNum = stoi(sanStr);
+                    if(sanStrNum < f[k-1][0] || sanStrNum > f[k-1][1]){
+                        continue;
+                    }
+                    string siStr = s.substr(i+j+k);
+                    if(siStr.length()>3){
+                        continue;
+                    }
+                    int siStrNum = stoi(siStr);
+                    if(siStrNum < f[2][0] || siStrNum > f[2][1]){
+                        continue;
+                    }
+                    result.push_back(firstStr+"."+secondStr+"."+sanStr+"."+siStr);
+                }
+            }
+        }
+        return result;
     }
+
 
     // vector<string> subToNumPieces(string s , int num){
     //     if(num > 1){
