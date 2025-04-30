b95a8be9498fa0a106c8546cffb266227cb63e35
dengbin <2776347262@qq.com>
2025-04-30 18:16:56 +0800

    JavaScript闭包test

--- js/workTest/test.js
+++ js/workTest/test.js
@@ -1,1 +1,15 @@
-console.log(1);
+function person(){
+    var age = 18;
+    function cat(){
+        age++;
+        console.log(age);
+    }
+    return cat;
+}
+console.log("1 ------ ");
+person()();// 19
+person()();// 19
+console.log("2 ------ ");
+let f = person();
+f();
+f();
