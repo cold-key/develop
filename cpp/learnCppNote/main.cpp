2fcb33f68af479d5cc27eaf4375d2aa529b31d98
dengbin <2776347262@qq.com>
2025-05-22 17:12:22 +0800

    笔记更新

--- cpp/learnCppNote/main.cpp
+++ cpp/learnCppNote/main.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 class A{
 public:
-    A(int a, int b):a(a),b(b){};
+    A(int a, int b, int c):a(a),b(b),c(c) {
+        cout << "A constructor" << endl;
+    };
+    void printSelf() {
+        cout << "a: " << a << " b: " << b << " c: " << c << endl;
+    };
     int a;
     int b;
+    const int c;
 };
 
 int main()
 {
     cout << "test"<<"\n";
+    A a(1,2,3);
+    a.printSelf();
     return 0;
 }
