69bcbeff93e873e87d97dc63699ad79c64da1bf8
dengbin <2776347262@qq.com>
2025-12-02 17:53:07 +0800

    # 滑动窗口基础题

---
+++ cpp/leetCode/cpp-leetcode/all/1052.爱生气的书店老板.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1052 lang=cpp
+ *
+ * [1052] 爱生气的书店老板
+ */
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int minC = 0;
+
+    }
+};
+// @lc code=end
+
