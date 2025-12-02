c05f8c68763d6339d298f8293c5303f140466824
db0189 <db0189@beavergame.com>
2025-12-02 09:57:30 +0800

    # 清空零散 添加TODO

---
+++ cpp/leetCode/cpp-leetcode/all/2141.同时运行-n-台电脑的最长时间.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=2141 lang=cpp
+ *
+ * [2141] 同时运行 N 台电脑的最长时间
+ */
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        //
+    }
+};
+// @lc code=end
+
--- note/TODAY_PLAN.md
+++ note/TODAY_PLAN.md
@@ -1,3 +1,5 @@
 1. 一天两道LeetCode Mid or Hard (C++)
 2. OpenGL -> Vulkan
 3. Unreal
+
+LeetCode: 385,2141
