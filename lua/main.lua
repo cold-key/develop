9c9384fdc99c93122238d13e31333761b0d6ab5f
dengbin <2776347262@qq.com>
2025-12-11 16:00:25 +0800

    # 项目结构调整

--- lua/main.lua
+++
@@ -1,4 +0,0 @@
--- require("lfs")
-
--- local currentDir = lfs.currentDir()
--- print(currentDir)
--- py/PokemmoScript/main.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print("Start sleeping")
-start_time = time.time()
-
-time.sleep(2)
-
-end_time = time.time()
-print(end_time - start_time)
