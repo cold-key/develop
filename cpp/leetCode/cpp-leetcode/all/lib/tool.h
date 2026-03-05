cc230be6df921d0ead45d0834082efe77c8ea922
dengbin <2776347262@qq.com>
2026-03-05 08:58:01 +0800

    # tool

--- cpp/leetCode/cpp-leetcode/all/lib/tool.h
+++ cpp/leetCode/cpp-leetcode/all/lib/tool.h
@@ -1,6 +1,16 @@
 #include<vector>
 #include<iostream>
 
+// LeetCode刷题本地调试用的tool.h
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 template <typename T>
 void printVector(std::vector<T> vector){
     for(auto it = vector.begin(); it != vector.end(); ++it){
@@ -8,3 +18,14 @@ void printVector(std::vector<T> vector){
     }
     std::cout << "\n" ;
 }
+
+void printLinkList(ListNode* head){
+    ListNode* p = head;
+    while(p != nullptr){
+        std::cout << p->val << " , ";
+        p = p->next;
+    }
+    std::cout << "\n";
+}
+
+// std::vector<int> getVector1ByLeetCodeStr
