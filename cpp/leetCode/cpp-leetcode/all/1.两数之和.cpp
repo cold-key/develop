441ea90014b39889c8c6572828994c04ab16abb8
YingYing <2776347262@qq.com>
2026-03-16 20:43:36 +0800

    # LeetCode New Device Try 1

---
+++ cpp/leetCode/cpp-leetcode/all/1.两数之和.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+ #include <unordered_map>
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // for (int i = 0; i < nums.size(); i++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // return {};
+
+        std::unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) { 
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end()) {
+                return {hash[complement], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+
+    }
+};
+// @lc code=end
+
