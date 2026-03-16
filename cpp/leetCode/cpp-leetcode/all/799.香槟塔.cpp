182a2173be87eef5c044061904e2fe2d839555bd
YingYing <2776347262@qq.com>
2026-03-16 22:01:46 +0800

    # LeetCode 799 No DO

--- cpp/leetCode/cpp-leetcode/all/799.香槟塔.cpp
+++ cpp/leetCode/cpp-leetcode/all/799.香槟塔.cpp
@@ -5,6 +5,7 @@
  */
 
  #include<iostream>
+ #include<queue>
 
 // @lc code=start
 class Solution {
@@ -14,14 +15,31 @@ public:
         double ans = 0;
         int i = 0;
         for(; remainingPoured > 0 ; i++){
-            std::cout << i << " , " << remainingPoured << std::endl;
+            // std::cout << i << " , " << remainingPoured << std::endl;
             if(query_row == i){
                 if(remainingPoured >= query_row + 1){
                     ans = 1;
                     break;
                 }else{
                     int tmp_i = i;
-                    // while()
+                    int tmp_j = query_glass;
+                    std::queue<std::pair<int, int>> q;
+                    q.push({tmp_i, tmp_j});
+                    while(!q.empty()){
+                        auto [cur_i, cur_j] = q.front();
+                        q.pop();
+                        if(cur_i == 0 || cur_j == 0 || cur_j == cur_i){
+                            std::cout << cur_i << " , " << cur_j << " : " << remainingPoured/(1<<cur_i)/(1<<(tmp_i - cur_i + 1)) << std::endl;
+                            ans += remainingPoured/(1<<cur_i)/(1<<(tmp_i - cur_i));
+                            continue;
+                        }
+                        if(cur_j > 0){
+                            q.push({cur_i - 1, cur_j - 1});
+                        }
+                        if(cur_j < cur_i){
+                            q.push({cur_i - 1, cur_j});
+                        }
+                    }
                     break;
                 }
             }else{
