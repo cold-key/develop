47ad33e9f2c144c8c2e69a43eca81f6d115438e7
db0190 <db0190@beavergame.com>
2026-03-19 09:55:33 +0800

    # 移动语义学习代码

--- cpp/learnCppNote/main.cpp
+++ cpp/learnCppNote/main.cpp
@@ -2,23 +2,30 @@
 
 using namespace std;
 
-class A{
+class Widget {
+    int* data;
 public:
-    A(int a, int b, int c):a(a),b(b),c(c) {
-        cout << "A constructor" << endl;
-    };
-    void printSelf() {
-        cout << "a: " << a << " b: " << b << " c: " << c << endl;
-    };
-    int a;
-    int b;
-    const int c;
+    Widget() : data(new int[1000]) { std::cout << "Default\n"; }
+    Widget(const Widget& w) : data(new int[1000]) {
+        std::copy(w.data, w.data+1000, data);
+        std::cout << "Copy\n";
+    }
+    Widget(Widget&& w) noexcept : data(w.data) {
+        w.data = nullptr;
+        std::cout << "Move\n";
+    }
+    ~Widget() { delete[] data; std::cout << "Destroy\n"; }
 };
 
-int main()
-{
-    cout << "test"<<"\n";
-    A a(1,2,3);
-    a.printSelf();
+Widget createWidget() {
+    Widget w;
+    return w;
+}
+
+int main() {
+    Widget w1;
+    Widget w2 = w1;
+    Widget w3 = createWidget();
+    Widget w4 = std::move(w1);
     return 0;
 }
