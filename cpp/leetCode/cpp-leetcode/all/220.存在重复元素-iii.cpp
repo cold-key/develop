f1b9f1adc59cad67f1f9b2a3cab811654a829152
db0190 <db0190@beavergame.com>
2026-03-20 17:58:34 +0800

    #LeetCode 220 Init

---
+++ cpp/leetCode/cpp-leetcode/all/220.存在重复元素-iii.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=220 lang=cpp
+ *
+ * [220] 存在重复元素 III
+ */
+
+#include<vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        
+    }
+};
+// @lc code=end
+
