f08b99b28b24115f77775d12403a031e10bc6d97
db0190 <db0190@beavergame.com>
2026-03-20 10:21:41 +0800

    # LearnOpenGL Hello Triangle End

--- graphicsAPI/LearnOpenGL/TutorialProjectSln/LearnOpenGLProject/main.cpp
+++ graphicsAPI/LearnOpenGL/TutorialProjectSln/LearnOpenGLProject/main.cpp
@@ -29,9 +29,12 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,   // 右上角
+		0.5f, -0.5f, 0.0f,  // 右下角
+		-0.5f, -0.5f, 0.0f, // 左下角
+		-0.5f, 0.5f, 0.0f,   // 左上角
+		0.5f, 0.5f, 0.0f,   // 右上角
+		-0.5f, -0.5f, 0.0f, // 左下角
 	};
 
 	unsigned int VAO;
@@ -41,6 +44,19 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int indices[] = {
+		// 注意索引从0开始! 
+		// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+		// 这样可以由下标代表顶点组合成矩形
+
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
+	unsigned int EBO;
+	glGenBuffers(1,&EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// 顶点着色器
 	const char* vertexShaderSource = "#version 330 core\n"
@@ -76,6 +92,22 @@ int main() {
 		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
 		std::cout << infoLog << std::endl;
 	}
+	// 片段着色器二
+	const char* fragmentShaderSource2 = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(0.4f, 0.5f, 0.2f, 1.0f);\n"
+		"}\0";
+	unsigned int fragmentShader2;
+	fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, nullptr);
+	glCompileShader(fragmentShader2);
+	glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader2, 512, nullptr, infoLog);
+		std::cout << infoLog << std::endl;
+	}
 	// 着色器程序
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
@@ -90,6 +122,7 @@ int main() {
 	glUseProgram(shaderProgram);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader2);
 	//
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -103,7 +136,10 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
