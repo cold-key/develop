d5cd01ce610bbb4ceabf57a708f745d6ea082979
db0190 <db0190@beavergame.com>
2026-03-24 16:56:47 +0800

    # LeetCode 344

---
+++ cpp/leetCode/cpp-leetcode/all/344.反转字符串.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=cpp
+ *
+ * [344] 反转字符串
+ */
+
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while(left < right){
+            std::swap(s[left],s[right]);
+            ++left;
+            --right;
+        }
+    }
+};
+// @lc code=end
+
