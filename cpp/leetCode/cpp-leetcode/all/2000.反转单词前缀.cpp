ca8ba2aa3d71ea3364998732039336361e9434a3
db0190 <db0190@beavergame.com>
2026-03-24 17:12:57 +0800

    # LeetCode 2000 easy

---
+++ cpp/leetCode/cpp-leetcode/all/2000.反转单词前缀.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=2000 lang=cpp
+ *
+ * [2000] 反转单词前缀
+ */
+
+#include<string>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        
+    }
+};
+// @lc code=end
+
