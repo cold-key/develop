1b8e9049b98de1d423fbde0cbda6704cf13a443b
db0190 <db0190@beavergame.com>
2026-04-07 15:10:05 +0800

    # LeetCode 2069 模拟 TODO

---
+++ cpp/leetCode/cpp-leetcode/all/2069.模拟行走机器人-ii.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=2069 lang=cpp
+ *
+ * [2069] 模拟行走机器人 II
+ */
+
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+// @lc code=start
+class Robot {
+public:
+    Robot(int width, int height) {
+        
+    }
+    
+    void step(int num) {
+        
+    }
+    
+    vector<int> getPos() {
+        
+    }
+    
+    string getDir() {
+        
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
+// @lc code=end
+
