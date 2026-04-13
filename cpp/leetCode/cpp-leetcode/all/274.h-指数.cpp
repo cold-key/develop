68aa0dad0f3326b19b97fe7c4638f649dc2f7f04
db0190 <db0190@beavergame.com>
2026-04-13 15:02:15 +0800

    # LeetCode 150题 274 添加二分法

--- cpp/leetCode/cpp-leetcode/all/274.h-指数.cpp
+++ cpp/leetCode/cpp-leetcode/all/274.h-指数.cpp
@@ -14,6 +14,11 @@ using namespace std;
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+        // return hIndexSortMethod(citations);
+        return hIndexBisectionMethod(citations);
+    }
+
+    int hIndexSortMethod(vector<int>& citations) {
         ranges::sort(citations);
         for(int i = 0; i < citations.size(); ++i){
             if(citations[i] >= citations.size() - i){
@@ -22,12 +27,32 @@ public:
         }
         return 0;
     }
+
+    int hIndexBisectionMethod(vector<int>& citations) {
+        int left = 0;
+        int right = citations.size();
+        while(left < right){
+            int mid = (left + right) / 2 + 1;
+            int cnt = 0;
+            for(int i = 0; i < citations.size(); ++i){
+                if(citations[i] >= mid){
+                    ++cnt;
+                }
+            }
+            if(cnt >= mid){
+                left = mid;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 };
 // @lc code=end
 
 int main(){
     Solution s;
-    vector<int> citations = {1,3,1};
+    vector<int> citations = {5,5,6,5,5};
     cout << s.hIndex(citations) << endl;
     return 0;
 }
