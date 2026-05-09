4745374c542302241ba3d84d7ec2ee3d84075939
db0190 <db0190@beavergame.com>
2026-05-09 17:56:56 +0800

    # LeetCode 42 修改

--- cpp/leetCode/cpp-leetcode/all/42.接雨水.cpp
+++ cpp/leetCode/cpp-leetcode/all/42.接雨水.cpp
@@ -14,21 +14,6 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int ans = 0;
-        int left = 0;
-        int right = left + 1;
-        int curAddValue = 0;
-        while(right < height.size()){
-            std::cout << curAddValue << " , ";
-            if(height[right] < height[left]){
-                curAddValue += height[left] - height[right];
-                ++right;
-            }else{
-                ans += std::max(curAddValue,0);
-                curAddValue = 0;
-                left = right;
-                ++right;
-            }
-        }
         return ans;
     }
 };
@@ -37,5 +22,5 @@ public:
 int main(){
     std::vector<int> height {0,1,0,2,1,0,1,3,2,1,2,1};
     Solution s;
-    s.trap(height);
+    std::cout << "\n" << s.trap(height);
 }
