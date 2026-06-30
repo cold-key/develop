4c087a4c4b1fee636b5cd07c1c98ba603745e4d1
db0190 <db0190@beavergame.com>
2026-06-30 14:13:52 +0800

    # tmp

---
+++ cpp/leetCode/cpp-leetcode/all/3558.给边赋权值的方案数-i.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=3558 lang=cpp
+ *
+ * [3558] 给边赋权值的方案数 I
+ */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        
+    }
+};
+// @lc code=end
+
