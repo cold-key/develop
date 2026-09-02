dacaa7fcd9b3af2a2b23a193f2c7f94b549f6145
db0190 <db0190@beavergame.com>
2026-09-02 10:58:20 +0800

    leetcode: 45

--- cpp/leetCode/cpp-leetcode/all/45.跳跃游戏-ii.cpp
+++ cpp/leetCode/cpp-leetcode/all/45.跳跃游戏-ii.cpp
@@ -12,7 +12,7 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int jump(vector<int>& nums) {
+    int jump2(vector<int>& nums) {
         int ans = 0;
         if(nums.size() <= 1){
             return ans;
@@ -34,12 +34,36 @@ public:
         }
         return ans;
     }
+
+    int jump(vector<int>& nums){
+        if(nums.size() <= 2){
+            return std::max(int(nums.size() - 1),0);
+        }
+        if(nums[0] >= nums.size() - 1){
+            return 1;
+        }
+        int curDealStep = 1;
+        int curStepFarIndex = nums[0];
+        int nextStepFarIndex = 0;
+        for(int i = 1; i < nums.size(); ++i){
+            nextStepFarIndex = std::max(nextStepFarIndex,i+nums[i]);
+            if(nextStepFarIndex >= nums.size() - 1){
+                break;
+            }
+            if(curStepFarIndex == i){
+                ++curDealStep;
+                curStepFarIndex = nextStepFarIndex;
+                nextStepFarIndex = 0;
+            }
+        }
+        return curDealStep + 1;
+    }
 };
 // @lc code=end
 
-// int main(){
-//     Solution s;
-//     vector<int> nums = {2,3,0,1,4};
-//     cout << s.jump(nums) << endl;
-//     return 0;
-// }
+int main(){
+    Solution s;
+    vector<int> nums = {2,3,1,1,4};
+    cout << s.jump2(nums) << endl;
+    return 0;
+}
